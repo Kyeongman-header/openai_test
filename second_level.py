4ad5041f59dfa1c68852f184a18f3ccdf32e5795
Kyeongman-header <zzangman2@gmail.com>
2023-02-25 18:27:24 +0900

    avg cross-attention of memory and gru cell implementation.

--- second_level.py
+++ second_level.py
@@ -19,7 +19,7 @@ def createFolder(directory):
 createFolder('second_level')
 PATH = './second_level/'+'all.tar'
 
-CONTINUOUSLY_TRAIN=True
+CONTINUOUSLY_TRAIN=False
 
 # train_total_target=last_target[:TRAIN_RANGE]
 # train_total_source=total_target[:TRAIN_RANGE]
@@ -32,7 +32,8 @@ CONTINUOUSLY_TRAIN=True
 # valid_dataset=return_dataset_2(val_total_target,val_total_source,val_total_prompt)
 
 
-with open("pickle_data/"+"train"+"/level_2.pickle","rb") as fi:
+#with open("pickle_data/"+"train"+"/level_2.pickle","rb") as fi:
+with open("pickle_data/"+"test_train"+"/level_2.pickle","rb") as fi:
         train_dataset = pickle.load(fi)
 with open("pickle_data/"+"valid"+"/level_2.pickle","rb") as fi:
         valid_dataset = pickle.load(fi)
@@ -56,19 +57,38 @@ class Network(nn.Module):
        self.shared = t5.get_input_embeddings()
        #nn.Embedding(config.vocab_size, config.d_model) 
        self.t5 = t5
+       self.grucell=nn.GRUCell(d_model,d_model).to('cuda:1')
+       """
+       self.wHr = nn.Linear(d_model,d_model).to('cuda:1')
+       self.wMr = nn.Linear(d_model,d_model).to('cuda:1')
+       self.wHz = nn.Linear(d_model,d_model).to('cuda:1')
+       self.wMz = nn.Linear(d_model,d_model).to('cuda:1')
+       self.wHn = nn.Linear(d_model,d_model).to('cuda:1')
+       self.wMn = nn.Linear(d_model,d_model).to('cuda:1')
+       """
 
-   def forward(self, input_ids,attention_mask,decoder_input_ids,labels,output_hidden_states,prev_predictions,prompt_ids,prompt_attention):
-
+   def forward(self, memory,input_ids,attention_mask,decoder_input_ids,labels,output_hidden_states,prev_predictions,prompt_ids,prompt_attention):
+       #memory states update.
        
+       
+       prev_predictions=torch.cat((torch.LongTensor([[tokenizer.pad_token_id]*(1024-prev_predictions.shape[1])]).to('cuda:1'),prev_predictions),1)
        prev_predictions = self.shared(prev_predictions)
+       #print(prev_predictions.shape)
+
        
+       memory=self.grucell(torch.squeeze(prev_predictions),torch.squeeze(memory)).unsqueeze(dim=0)
+       
+       print(memory.shape)
+
        #print("embedded prev prediction : ")
        #print(prev_predictions.shape)
        
-       prev_predictions = torch.mean(prev_predictions,dim=-2,keepdim=True)
-       
-       #print("avg embedded prev prediction : ")
-       #print(prev_predictions.shape)
+       # prev_predictions = torch.mean(prev_predictions,dim=-2,keepdim=True)
+
+       # prev_predictions을 mean때리지 않고 마치 decoder에 encoder output을 prompt로 넣듯이 똑같이 해보자. 
+
+       print("embedded prev prediction : ")
+       print(prev_predictions.shape)
        
 
        #print("before concat, prompt ids shape :")
@@ -85,23 +105,28 @@ class Network(nn.Module):
        #print("concat attention mask shape : ")
        #print(attention_mask.shape)
        
-       inputs_embeds=torch.cat((prev_predictions,inputs_embeds),1)
-       attention_mask=torch.cat((torch.LongTensor([[1]]).to('cuda:1'),attention_mask),1)
+       #inputs_embeds=torch.cat((prev_predictions,inputs_embeds),1)
+       #attention_mask=torch.cat((torch.LongTensor([[1]*prev_predictions.shape[1]]).to('cuda:1'),attention_mask),1)
 
        #print("prev concat input embeds shape : ")
        #print(inputs_embeds.shape)
        #print("prev concat attention mask shape : ")
        #print(attention_mask.shape)
-       outputs = self.t5(input_ids = None,inputs_embeds=inputs_embeds,attention_mask = attention_mask,decoder_input_ids = decoder_input_ids,labels=decoder_input_ids,output_hidden_states=True)
-       return outputs
+       outputs = self.t5(input_ids = None,inputs_embeds=inputs_embeds,attention_mask = attention_mask,decoder_input_ids = decoder_input_ids,labels=decoder_input_ids,output_hidden_states=True,memory=memory)
+       return outputs,memory
     
-   def generate(self, input_ids,attention_mask,decoder_input_ids,labels,output_hidden_states,prev_predictions,prompt_ids,prompt_attention):
+   def generate(self, memory,input_ids,attention_mask,decoder_input_ids,labels,output_hidden_states,prev_predictions,prompt_ids,prompt_attention):
+       
+       prev_predictions=torch.cat((torch.LongTensor([[tokenizer.pad_token_id]*(1024-prev_predictions.shape[1])]).to('cuda:1'),prev_predictions),1)
        prev_predictions = self.shared(prev_predictions)
+       #print(prev_predictions.shape)
 
+
+       memory=self.grucell(torch.squeeze(prev_predictions),torch.squeeze(memory)).unsqueeze(dim=0)
        #print("embedded prev prediction : ")
        #print(prev_predictions.shape)
 
-       prev_predictions = torch.mean(prev_predictions,dim=-2,keepdim=True)
+       #prev_predictions = torch.mean(prev_predictions,dim=-2,keepdim=True)
 
        #print("avg embedded prev prediction : ")
        #print(prev_predictions.shape)
@@ -121,19 +146,21 @@ class Network(nn.Module):
        #print("concat attention mask shape : ")
        #print(attention_mask.shape)
 
-       inputs_embeds=torch.cat((prev_predictions,inputs_embeds),1)
-       attention_mask=torch.cat((torch.LongTensor([[1]]).to('cuda:1'),attention_mask),1)
+       #inputs_embeds=torch.cat((prev_predictions,inputs_embeds),1)
+       #attention_mask=torch.cat((torch.LongTensor([[1]*prev_predictions.shape[1]]).to('cuda:1'),attention_mask),1)
        dummy_decoder_input_ids = torch.tensor([[tokenizer.pad_token_id]]).to('cuda:1')
        source= tokenizer.batch_decode(input_ids,skip_special_tokens=True)
        print("source")
        print(source)
-       return self.t5.generate(inputs_embeds=inputs_embeds,attention_mask=attention_mask,decoder_input_ids=dummy_decoder_input_ids)
+       return self.t5.generate(max_length=512,memory=memory,inputs_embeds=inputs_embeds,attention_mask=attention_mask,decoder_input_ids=dummy_decoder_input_ids),memory
 
 config = AutoConfig.from_pretrained('facebook/bart-base',gradient_checkpointing=True)
 if CONTINUOUSLY_TRAIN:
     t5 =  AutoModelForSeq2SeqLM.from_config(config).to('cuda:1') # 이후부터는 내가 finetune한 t5를 사용(밑에서 torch로 불러온다.)
 else:
     t5 = AutoModelForSeq2SeqLM.from_pretrained('facebook/bart-base').to('cuda:1') # 최초 학습에서는 pretrained 된 t5를 사용
+
+t5.get_input_embeddings().requires_grad = False # embedding layer는 학습을 안한다. 얘가 변동되면 prev_predictions에 대한 표현도 계속 변하기 때문.
 
 model = Network(config.vocab_size, config.d_model,t5).to('cuda:1')
 
@@ -175,11 +202,13 @@ def trainer():
         
             input_ids=input_ids.to('cuda:1')
             attention_mask=attention_mask.to('cuda:1')
-        
+            
+
+            memory = torch.zeros_like(torch.empty(1,1024,config.d_model)).to('cuda:1') # first memory.
         #print(prev_predictions)        
             for d in num_decoder_input_ids:
 
-            #input()
+                #input()
                 prev_predictions=prev_predictions.to('cuda:1')
                 
                 word=""
@@ -211,13 +240,14 @@ def trainer():
                 optimizer.zero_grad()
 
         # forward + backward + optimize
-                outputs = model(input_ids = input_ids,attention_mask = attention_mask,decoder_input_ids = d,labels=d,output_hidden_states=True,prev_predictions=prev_predictions,prompt_ids=prompt_ids,prompt_attention=prompt_attention)
-
+                
+                outputs,memory = model(memory=memory.detach(),input_ids = input_ids,attention_mask = attention_mask,decoder_input_ids = d,labels=d,output_hidden_states=True,prev_predictions=prev_predictions,prompt_ids=prompt_ids,prompt_attention=prompt_attention) # 중요! memory.detach()를 하지 않으면 매번 memory cell에 대한 gradient는 계속 이어져나가 계산되기 때문에, 두번 그래디언트 업데이트 했다고 오류 뜬다.
+                
                 loss = outputs.loss
                 loss.backward()
-            
+                
                 prev_predictions =  torch.argmax(outputs.logits, dim=-1)
-
+                
                 optimizer.step()
                 lr_scheduler.step()
                 optimizer.zero_grad()
@@ -246,13 +276,14 @@ def trainer():
 # -----------train ends, eval starts.
 f = open('second_level_val_results.csv','w', newline='')
 wr = csv.writer(f)
-wr.writerow(["index","whole text","generated_results"])
+wr.writerow(["index","source","real text","generated_results"])
 index=0
 
 import evaluate
 
 metric = evaluate.load("rouge")
 model.eval()
+
 for data in valid_dataset:
     input_ids,attention_mask,num_decoder_input_ids = (data['input_ids'],data['input_attention'],data['decoder_input_ids'])
     
@@ -263,7 +294,7 @@ for data in valid_dataset:
 
     input_ids=input_ids.to('cuda:1')
     attention_mask=attention_mask.to('cuda:1')
-
+    memory = torch.zeros_like(torch.empty(1,1024,config.d_model)).to('cuda:1') # first memory.
         #print(prev_predictions)
     for d in num_decoder_input_ids:
 
@@ -299,7 +330,7 @@ for data in valid_dataset:
 
         #prev_predictions =  torch.argmax(outputs.logits, dim=-1)
         
-        outputs=model.generate(input_ids = input_ids,attention_mask = attention_mask,decoder_input_ids = ex_d,labels=ex_d,output_hidden_states=True,
+        outputs,memory=model.generate(memory=memory.detach(),input_ids = input_ids,attention_mask = attention_mask,decoder_input_ids = ex_d,labels=ex_d,output_hidden_states=True,
                     prev_predictions=prev_predictions,prompt_ids=prompt_ids,prompt_attention=prompt_attention)
         prev_predictions = outputs # 이렇게 만들면 outputs에 id가 나오는 모양임.
         #predictions = torch.argmax(logits, dim=-1)
@@ -311,7 +342,7 @@ for data in valid_dataset:
         ex_d = tokenizer.batch_decode(ex_d,skip_special_tokens=True)
         print("golden label")
         print(ex_d) 
-        wr.writerow([str(index),ex_d,predictions])
+        wr.writerow([str(index),tokenizer.batch_decode(input_ids,skip_special_tokens=True),ex_d,predictions])
         index+=1
         metric.add_batch(predictions=predictions, references=ex_d)
 
