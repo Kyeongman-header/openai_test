2a0b7836ff96f357a41bfa2e19bddbf4b8ab4ec7
Kyeongman-header <zzangman2@gmail.com>
2023-03-15 11:55:34 +0900

    rocstories add

--- rocstories.py
+++ rocstories.py
@@ -19,10 +19,15 @@ decoder_max_length = 1024
 batch_size = 1
 
 file="ROCStories_train"
+file2="ROCStories__spring2016 - ROCStories_spring2016"
 
 
-f = open(file+'.csv', 'r', encoding='utf-8')
-rdr = csv.reader(f)
+f = pd.read_csv(file+'.csv',chunksize=1000)
+f= pd.concat(f)
+f2 = pd.read_csv(file2+'.csv',chunksize=1000)
+f2 = pd.concat(f2)
+# open(file+'.csv', 'r', encoding='utf-8')
+# rdr = csv.reader(f)
 
 
 first=True
@@ -41,7 +46,60 @@ prompt_max=0
 prompt_min=999999
 prompt_len_arr=[]
 
-for line in rdr:
+for index, line in tqdm(f.iterrows()):
+        #print(line)
+        #total_target.append(print(line[1]))
+        #total_source.append(line[1])
+    
+    if first:
+        first=False
+        continue
+#    count+=1
+#    if count>100:
+#        break
+    # print(line[0])
+    # print(line[1])
+    # print(line[2])
+    # print(line[3])
+
+    #text=line[2]+" "+line[3]+" "+line[4]+" "+line[5]+" "+line[6]
+    text=line[3]+" "+line[4]+" "+line[5]+" "+line[6]
+    text_len=len(tokenizer(text).input_ids)
+
+    prompt=line[1] + ". " + line[2]
+    prompt_len=len(tokenizer(prompt).input_ids)
+    
+    text_len_arr.append(text_len)
+    prompt_len_arr.append(prompt_len)
+
+    num+=1
+    # print("summary : ")
+    # print(summary)
+    # print("len : ")
+    # print(summary_len)
+    prompt_max=max(prompt_max,prompt_len)
+    prompt_min=min(prompt_min,prompt_len)
+    prompt_avg+=int(prompt_len)
+
+    # print("analysis : ")
+    # print(anal)
+    # print("len : ")
+    # print(anal_len)
+
+    text_max=max(text_max,text_len)
+    text_min=min(text_min,text_len)
+    text_avg+=int(text_len)
+    
+    # print("text : ")
+    # print(chapter)
+    # print("len : ")
+    # print(chapter_len)
+
+    stories.append(text)
+    titles.append(prompt)
+    
+
+for index, line in tqdm(f2.iterrows()):
         #print(line)
         #total_target.append(print(line[1]))
         #total_source.append(line[1])
