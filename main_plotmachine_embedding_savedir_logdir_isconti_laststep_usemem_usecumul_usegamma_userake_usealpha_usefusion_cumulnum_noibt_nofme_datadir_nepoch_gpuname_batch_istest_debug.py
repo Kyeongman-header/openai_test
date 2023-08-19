126e931e965c3aeb721b791989f60870942907fa
Kyeongman-header <zzangman2@gmail.com>
2023-08-19 15:04:31 +0900

    main_plot

--- main_plotmachine_embedding_savedir_logdir_isconti_laststep_usemem_usecumul_usegamma_userake_usealpha_usefusion_cumulnum_noibt_nofme_datadir_nepoch_gpuname_batch_istest_debug.py
+++ main_plotmachine_embedding_savedir_logdir_isconti_laststep_usemem_usecumul_usegamma_userake_usealpha_usefusion_cumulnum_noibt_nofme_datadir_nepoch_gpuname_batch_istest_debug.py
@@ -798,7 +798,7 @@ class Network(nn.Module):
             #         #encoder_no_repeat_ngram_size=3,
             #         repetition_penalty=3.5,early_stopping=True,context=cumulation[b],alpha=alpha[b],beta=beta[b]))
             outputs.append(self.gpt.generate(max_new_tokens=350,memory=one_memory,
-                                             preceding_context=torch.cat([torch.unsqueeze(avg_context[b],dim=0)]*self.num_beams,dim=0),
+                                             preceding_context=torch.stack([torch.unsqueeze(avg_context[b],dim=0)]*self.num_beams,dim=0),
                                              input_ids=input_id,
                         #attention_mask=attention_mask[b],
                         num_beams=self.num_beams,
