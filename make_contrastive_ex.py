f3f2f843ac692b46af95f091e61d6740f5881971
Kyeongman-header <zzangman2@gmail.com>
2023-10-03 11:10:41 +0000

    make_contrastive

--- make_contrastive_ex.py
+++ make_contrastive_ex.py
@@ -414,7 +414,7 @@ def making_nextsentenceprediction_examples(new_whole_data):
                 second_sample_parag_num=sample_parag_num
                 sample_parag_num=temp #(둘의 순서를 바꾼다. 1-0 이렇게.)
             """ 
-            neg_sample += "[SEP]" + sample[i][sample_parag_num-1] # 이러면 원래 이어져있어야 하는 두 문단이
+            neg_sample += "[SEP]" + " " + sample[i][sample_parag_num-1] # 이러면 원래 이어져있어야 하는 두 문단이
             # 반대 순서로 엮여 있게 된다.
 
             neg_examples_3.append({'data' : neg_sample,'label':[0]})
@@ -424,7 +424,7 @@ def making_nextsentenceprediction_examples(new_whole_data):
             
             sample_parag_num=random.randint(0,len(sample[j])-2) # 예를들어 길이가 3이면, 0~1까지 랜덤한 문단 하나를 뽑는다. (마지막 문단만 빼고.)
             pos_sample=sample[j][sample_parag_num]
-            pos_sample += "[SEP]" + sample[j][sample_parag_num+1]
+            pos_sample += "[SEP]" + " " + sample[j][sample_parag_num+1]
             
             pos_examples_3.append({'data' : pos_sample,'label':[1]})
     
--- make_contrastive_ex.sh
+++ make_contrastive_ex.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 #python make_con* valid wprdbk 0 1
 #python make_con* train wp 0 1
-python make_con* train wp 100000 2
-python make_con* train wp 200000 3
-python make_con* train rd 0 4
-python make_con* train bk 0 5
-sh run.sh 
+python make_contrastive*.py test nextsentenceprediction whole
+python make_contrastive*.py train nextsentenceprediction whole
+python make_contrastive*.py valid nextsentenceprediction whole
+#python make_contrastive*.py test completeness whole
+#python make_contrastive*.py train completeness whole
+#python make_contrastive*.py valid completeness whole
+sh gpt_run.sh
