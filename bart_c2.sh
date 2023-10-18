610600dd3843ecef062eff6407c259daee0ac7f9
Kyeongman-header <zzangman2@gmail.com>
2023-10-18 11:36:53 +0900

    c1c2c3

---
+++ bart_c2.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+
+python main_bart_embedding_savedir_logdir_isconti_laststep_usemem_usecumul_usegamma_userake_usealpha_usefusion_cumulnum_noibt_nofme_datadir_nepoch_gpuname_batch_istest_debug.py bart_rake_c2 bart_rake_c2 0 0 1 1 0 0 1 0 1 0 0 whole 2 cuda:0 4 0 0
+
+python main_bart_embedding_savedir_logdir_isconti_laststep_usemem_usecumul_usegamma_userake_usealpha_usefusion_cumulnum_noibt_nofme_datadir_nepoch_gpuname_batch_istest_debug.py bart_rake_c2 bart_rake_c2 0 0 1 1 0 0 1 0 1 0 0 whole 1 cuda:0 1 1 0
+
+python test_longformer_filename_savedir_logdir_gpu_debug.py bartGenerations/bart_rake_c2/test/generations_outputs_5 completeness-gpt bart_c2_5_completeness-gpt cuda:0 5 0
+
+python test_longformer_filename_savedir_logdir_gpu_debug.py bartGenerations/bart_rake_c2/test/generations_outputs_10 completeness-gpt bart_c2_10_completeness-gpt cuda:0 10 0
+
+python test_longformer_filename_savedir_logdir_gpu_debug.py bartGenerations/bart_rake_c2/test/generations_outputs_19 completeness-gpt bart_c2_19_completeness-gpt cuda:0 19 0
+
+python test_longformer_filename_savedir_logdir_gpu_debug.py bartGenerations/bart_rake_c2/test/generations_outputs_30 completeness-gpt bart_c2_30_completeness-gpt cuda:0 30 0
+
+python test_longformer_filename_savedir_logdir_gpu_debug.py bartGenerations/bart_rake_c2/test/generations_outputs_50 completeness-gpt bart_c2_50_completeness-gpt cuda:0 50 0
+
+
+python test_longformer_filename_savedir_logdir_gpu_debug.py bartGenerations/bart_rake_c2/test/generations_outputs_5 nextsentenceprediction-gpt bart_c2_5_nextsentenceprediction-gpt cuda:0 5 0
+
+python test_longformer_filename_savedir_logdir_gpu_debug.py bartGenerations/bart_rake_c2/test/generations_outputs_10 nextsentenceprediction-gpt bart_c2_10_nextsentenceprediction-gpt cuda:0 10 0
+
+python test_longformer_filename_savedir_logdir_gpu_debug.py bartGenerations/bart_rake_c2/test/generations_outputs_19 nextsentenceprediction-gpt bart_c2_19_nextsentenceprediction-gpt cuda:0 19 0
+
+python test_longformer_filename_savedir_logdir_gpu_debug.py bartGenerations/bart_rake_c2/test/generations_outputs_30 nextsentenceprediction-gpt bart_c2_30_nextsentenceprediction-gpt cuda:0 30 0
+
+python test_longformer_filename_savedir_logdir_gpu_debug.py bartGenerations/bart_rake_c2/test/generations_outputs_50 nextsentenceprediction-gpt bart_c2_50_nextsentenceprediction-gpt cuda:0 50 0
+
+
