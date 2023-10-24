7ee838f6c1c50fdf0a1790bd5684ba584d5c614d
Kyeongman-header <zzangman2@gmail.com>
2023-10-24 22:54:41 +0900

    pickle dataset maker

--- only_make_pickle_dataset.py
+++ only_make_pickle_dataset.py
@@ -3,14 +3,24 @@ import ctypes as ct
 import math
 import numpy as np
 csv.field_size_limit(int(ct.c_ulong(-1).value // 2))
-from dataset_consts import *
+
+import sys
+
+trainvalidtest=sys.argv[1] # train
+wprdbk=sys.argv[2] # wp / booksum / reedsy
+bartgpt=sys.argv[3] # gpt / bart
+
+if "gpt" in bartgpt:
+    from dataset_consts_gpt import *
+else:
+    from dataset_consts_bart import *
 
 total_target=[]
 total_source=[]
 last_target=[]
 
-file="test" # valid이면 밑에 file 이름 바꿔주기.
-f = open(file+'_wp_rake_results.csv', 'r', encoding='utf-8')
+file=trainvalidtest # valid이면 밑에 file 이름 dev로 바꾸기.
+f = open(file+'_'+wprdbk+'_rake_results.csv', 'r', encoding='utf-8')
 rdr = csv.reader(f)
 first=True
 
@@ -25,15 +35,12 @@ for line in rdr:
     if first:
         first=False
         continue
-#    count+=1
+    count+=1
 #    if count>100:
 #        break
-    print(line[0])
-    print(line[1])
-    print(line[2])
     last_target.append(line[0])
     total_target.append(line[1])
     total_source.append(line[2])
-    input()
+    print(count,end='\r')
 
 save_tokenize_pickle_data_2(file,total_source,total_target,last_target)
