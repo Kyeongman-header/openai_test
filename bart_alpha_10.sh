32b7e175d78bc5b86a10ce8a103fdb0ef53dc115
Kyeongman-header <zzangman2@gmail.com>
2023-10-28 14:13:33 +0900

    alpha test

---
+++ bart_alpha_10.sh
@@ -0,0 +1,39 @@
+#!/bin/bash0
+#SBATCH --job-name  alpha10
+#SBATCH --time      96:00:00
+#SBATCH -c          10
+#SBATCH --mem       20G
+#SBATCH --gpus      1
+#SBATCH --mail-type END
+#SBATCH --mail-user zzangmane@snu.ac.kr
+conda activate torch
+ml cuda
+
+python main_bart_embedding_savedir_logdir_isconti_laststep_usemem_usecumul_usegamma_userake_usealpha_usefusion_cumulnum_noibt_nofme_datadir_nepoch_gpuname_batch_istest_debug.py bart_alpha10 bart_alpha10 0 0 1 1 0 0 1 0 1 0 0 whole 1 cuda:0 8 0 0 0.1
+
+python main_bart_embedding_savedir_logdir_isconti_laststep_usemem_usecumul_usegamma_userake_usealpha_usefusion_cumulnum_noibt_nofme_datadir_nepoch_gpuname_batch_istest_debug.py bart_alpha10 bart_alpha10 0 0 1 1 0 0 1 0 1 0 0 whole 1 cuda:0 1 1 0 0.1
+
+python test_longformer_filename_savedir_logdir_gpu_debug.py bartGenerations/bart_alpha10/test/generations_outputs_2 completeness-gpt bart_alpha10_2_completeness-gpt cuda:0 2 0
+
+python test_longformer_filename_savedir_logdir_gpu_debug.py bartGenerations/bart_alpha10/test/generations_outputs_5 completeness-gpt bart_alpha10_5_completeness-gpt cuda:0 5 0
+
+python test_longformer_filename_savedir_logdir_gpu_debug.py bartGenerations/bart_alpha10/test/generations_outputs_10 completeness-gpt bart_alpha10_10_completeness-gpt cuda:0 10 0
+
+python test_longformer_filename_savedir_logdir_gpu_debug.py bartGenerations/bart_alpha10/test/generations_outputs_15 completeness-gpt bart_alpha10_15_completeness-gpt cuda:0 15 0
+
+python test_longformer_filename_savedir_logdir_gpu_debug.py bartGenerations/bart_alpha10/test/generations_outputs_20 completeness-gpt bart_alpha10_20_completeness-gpt cuda:0 20 0
+
+python test_longformer_filename_savedir_logdir_gpu_debug.py bartGenerations/bart_alpha10/test/generations_outputs_25 completeness-gpt bart_alpha10_25_completeness-gpt cuda:0 25 0
+
+
+python test_longformer_filename_savedir_logdir_gpu_debug.py bartGenerations/bart_alpha10/test/generations_outputs_2 nextsentenceprediction-gpt bart_alpha10_2_nextsentenceprediction-gpt cuda:0 2 0
+
+python test_longformer_filename_savedir_logdir_gpu_debug.py bartGenerations/bart_alpha10/test/generations_outputs_5 nextsentenceprediction-gpt bart_alpha10_5_nextsentenceprediction-gpt cuda:0 5 0
+
+python test_longformer_filename_savedir_logdir_gpu_debug.py bartGenerations/bart_alpha10/test/generations_outputs_10 nextsentenceprediction-gpt bart_alpha10_10_nextsentenceprediction-gpt cuda:0 10 0
+
+python test_longformer_filename_savedir_logdir_gpu_debug.py bartGenerations/bart_alpha10/test/generations_outputs_15 nextsentenceprediction-gpt bart_alpha10_15_nextsentenceprediction-gpt cuda:0 15 0
+
+python test_longformer_filename_savedir_logdir_gpu_debug.py bartGenerations/bart_alpha10/test/generations_outputs_20 nextsentenceprediction-gpt bart_alpha10_20_nextsentenceprediction-gpt cuda:0 20 0
+
+python test_longformer_filename_savedir_logdir_gpu_debug.py bartGenerations/bart_alpha10/test/generations_outputs_25 nextsentenceprediction-gpt bart_alpha10_25_nextsentenceprediction-gpt cuda:0 25 0
---
+++ bart_alpha_20.sh
@@ -0,0 +1,39 @@
+#!/bin/bash0
+#SBATCH --job-name  alpha20
+#SBATCH --time      96:00:00
+#SBATCH -c          10
+#SBATCH --mem       20G
+#SBATCH --gpus      1
+#SBATCH --mail-type END
+#SBATCH --mail-user zzangmane@snu.ac.kr
+conda activate torch
+ml cuda
+
+python main_bart_embedding_savedir_logdir_isconti_laststep_usemem_usecumul_usegamma_userake_usealpha_usefusion_cumulnum_noibt_nofme_datadir_nepoch_gpuname_batch_istest_debug.py bart_alpha20 bart_alpha20 0 0 1 1 0 0 1 0 1 0 0 whole 1 cuda:0 8 0 0 0.1
+
+python main_bart_embedding_savedir_logdir_isconti_laststep_usemem_usecumul_usegamma_userake_usealpha_usefusion_cumulnum_noibt_nofme_datadir_nepoch_gpuname_batch_istest_debug.py bart_alpha20 bart_alpha20 0 0 1 1 0 0 1 0 1 0 0 whole 1 cuda:0 1 1 0 0.1
+
+python test_longformer_filename_savedir_logdir_gpu_debug.py bartGenerations/bart_alpha20/test/generations_outputs_2 completeness-gpt bart_alpha20_2_completeness-gpt cuda:0 2 0
+
+python test_longformer_filename_savedir_logdir_gpu_debug.py bartGenerations/bart_alpha20/test/generations_outputs_5 completeness-gpt bart_alpha20_5_completeness-gpt cuda:0 5 0
+
+python test_longformer_filename_savedir_logdir_gpu_debug.py bartGenerations/bart_alpha20/test/generations_outputs_10 completeness-gpt bart_alpha20_10_completeness-gpt cuda:0 10 0
+
+python test_longformer_filename_savedir_logdir_gpu_debug.py bartGenerations/bart_alpha20/test/generations_outputs_15 completeness-gpt bart_alpha20_15_completeness-gpt cuda:0 15 0
+
+python test_longformer_filename_savedir_logdir_gpu_debug.py bartGenerations/bart_alpha20/test/generations_outputs_20 completeness-gpt bart_alpha20_20_completeness-gpt cuda:0 20 0
+
+python test_longformer_filename_savedir_logdir_gpu_debug.py bartGenerations/bart_alpha20/test/generations_outputs_25 completeness-gpt bart_alpha20_25_completeness-gpt cuda:0 25 0
+
+
+python test_longformer_filename_savedir_logdir_gpu_debug.py bartGenerations/bart_alpha20/test/generations_outputs_2 nextsentenceprediction-gpt bart_alpha20_2_nextsentenceprediction-gpt cuda:0 2 0
+
+python test_longformer_filename_savedir_logdir_gpu_debug.py bartGenerations/bart_alpha20/test/generations_outputs_5 nextsentenceprediction-gpt bart_alpha20_5_nextsentenceprediction-gpt cuda:0 5 0
+
+python test_longformer_filename_savedir_logdir_gpu_debug.py bartGenerations/bart_alpha20/test/generations_outputs_10 nextsentenceprediction-gpt bart_alpha20_10_nextsentenceprediction-gpt cuda:0 10 0
+
+python test_longformer_filename_savedir_logdir_gpu_debug.py bartGenerations/bart_alpha20/test/generations_outputs_15 nextsentenceprediction-gpt bart_alpha20_15_nextsentenceprediction-gpt cuda:0 15 0
+
+python test_longformer_filename_savedir_logdir_gpu_debug.py bartGenerations/bart_alpha20/test/generations_outputs_20 nextsentenceprediction-gpt bart_alpha20_20_nextsentenceprediction-gpt cuda:0 20 0
+
+python test_longformer_filename_savedir_logdir_gpu_debug.py bartGenerations/bart_alpha20/test/generations_outputs_25 nextsentenceprediction-gpt bart_alpha20_25_nextsentenceprediction-gpt cuda:0 25 0
