d2e07e83ee8c4eb041169baaf0b472e8bdb010df
Kyeongman-header <zzangman2@gmail.com>
2023-11-08 18:15:27 +0900

    200

--- cal_rouges.sh
+++ cal_rouges.sh
@@ -9,52 +9,52 @@
 conda activate torch
 ml cuda
 
-python cal_rouge.py bartGenerations/bart_rake_lp/test/generations_outputs_5 5
-python cal_rouge.py bartGenerations/bart_rake_lp/test/generations_outputs_19 19
-python cal_rouge.py bartGenerations/bart_rake_lp/test/generations_outputs_30 30
-python cal_rouge.py bartGenerations/bart_rake_lp/test/generations_outputs_50 50
+# python cal_rouge.py bartGenerations/bart_rake_lp/test/generations_outputs_5 5
+# python cal_rouge.py bartGenerations/bart_rake_lp/test/generations_outputs_19 19
+# python cal_rouge.py bartGenerations/bart_rake_lp/test/generations_outputs_30 30
+# python cal_rouge.py bartGenerations/bart_rake_lp/test/generations_outputs_50 50
 
-python cal_rouge.py bartGenerations/bart_rake_nolp/test/generations_outputs_5 5
-python cal_rouge.py bartGenerations/bart_rake_nolp/test/generations_outputs_30 30
-python cal_rouge.py bartGenerations/bart_rake_nolp/test/generations_outputs_50 50
+# python cal_rouge.py bartGenerations/bart_rake_nolp/test/generations_outputs_5 5
+# python cal_rouge.py bartGenerations/bart_rake_nolp/test/generations_outputs_30 30
+# python cal_rouge.py bartGenerations/bart_rake_nolp/test/generations_outputs_50 50
 
-python cal_rouge.py bartGenerations/bart_rake/test/generations_outputs_19 19
-python cal_rouge.py bartGenerations/bart_rake/test/generations_outputs_30 30
-python cal_rouge.py bartGenerations/bart_rake/test/generations_outputs_50 50
+# python cal_rouge.py bartGenerations/bart_rake/test/generations_outputs_19 19
+# python cal_rouge.py bartGenerations/bart_rake/test/generations_outputs_30 30
+# python cal_rouge.py bartGenerations/bart_rake/test/generations_outputs_50 50
 
-python cal_rouge.py PlotmachineGenerations/plotmachine_rake/test/generations_outputs_5 5
-python cal_rouge.py PlotmachineGenerations/plotmachine_rake/test/generations_outputs_19 19
-python cal_rouge.py PlotmachineGenerations/plotmachine_rake/test/generations_outputs_30 30
-python cal_rouge.py PlotmachineGenerations/plotmachine_rake/test/generations_outputs_50 50
+# python cal_rouge.py PlotmachineGenerations/plotmachine_rake/test/generations_outputs_5 5
+# python cal_rouge.py PlotmachineGenerations/plotmachine_rake/test/generations_outputs_19 19
+# python cal_rouge.py PlotmachineGenerations/plotmachine_rake/test/generations_outputs_30 30
+# python cal_rouge.py PlotmachineGenerations/plotmachine_rake/test/generations_outputs_50 50
 
-python cal_rouge.py GPTGenerations/gpt_nomemnocumul_alpha/test/generations_outputs_5 5
-python cal_rouge.py GPTGenerations/gpt_nomemnocumul_alpha/test/generations_outputs_19 19
-python cal_rouge.py GPTGenerations/gpt_nomemnocumul_alpha/test/generations_outputs_30 30
-python cal_rouge.py GPTGenerations/gpt_nomemnocumul_alpha/test/generations_outputs_50 50
+# python cal_rouge.py GPTGenerations/gpt_nomemnocumul_alpha/test/generations_outputs_5 5
+# python cal_rouge.py GPTGenerations/gpt_nomemnocumul_alpha/test/generations_outputs_19 19
+# python cal_rouge.py GPTGenerations/gpt_nomemnocumul_alpha/test/generations_outputs_30 30
+# python cal_rouge.py GPTGenerations/gpt_nomemnocumul_alpha/test/generations_outputs_50 50
 
-python cal_rouge.py bartGenerations/bart_nomem_rake/test/generations_outputs_5 5
-python cal_rouge.py bartGenerations/bart_nomem_rake/test/generations_outputs_10 10
-python cal_rouge.py bartGenerations/bart_nomem_rake/test/generations_outputs_19 19
-python cal_rouge.py bartGenerations/bart_nomem_rake/test/generations_outputs_30 30
-python cal_rouge.py bartGenerations/bart_nomem_rake/test/generations_outputs_50 50
+# python cal_rouge.py bartGenerations/bart_nomem_rake/test/generations_outputs_5 5
+# python cal_rouge.py bartGenerations/bart_nomem_rake/test/generations_outputs_10 10
+# python cal_rouge.py bartGenerations/bart_nomem_rake/test/generations_outputs_19 19
+# python cal_rouge.py bartGenerations/bart_nomem_rake/test/generations_outputs_30 30
+# python cal_rouge.py bartGenerations/bart_nomem_rake/test/generations_outputs_50 50
 
-python cal_rouge.py bartGenerations/bart_nocumul_rake/test/generations_outputs_5 5
-python cal_rouge.py bartGenerations/bart_nocumul_rake/test/generations_outputs_19 19
-python cal_rouge.py bartGenerations/bart_nocumul_rake/test/generations_outputs_30 30
-python cal_rouge.py bartGenerations/bart_nocumul_rake/test/generations_outputs_50 50
+# python cal_rouge.py bartGenerations/bart_nocumul_rake/test/generations_outputs_5 5
+# python cal_rouge.py bartGenerations/bart_nocumul_rake/test/generations_outputs_19 19
+# python cal_rouge.py bartGenerations/bart_nocumul_rake/test/generations_outputs_30 30
+# python cal_rouge.py bartGenerations/bart_nocumul_rake/test/generations_outputs_50 50
 
-python cal_rouge.py bartGenerations/bart_nomemnocumul_rake/test/generations_outputs_5 5
-python cal_rouge.py bartGenerations/bart_nomemnocumul_rake/test/generations_outputs_30 30
-python cal_rouge.py bartGenerations/bart_nomemnocumul_rake/test/generations_outputs_50 50
+# python cal_rouge.py bartGenerations/bart_nomemnocumul_rake/test/generations_outputs_5 5
+# python cal_rouge.py bartGenerations/bart_nomemnocumul_rake/test/generations_outputs_30 30
+# python cal_rouge.py bartGenerations/bart_nomemnocumul_rake/test/generations_outputs_50 50
 
-python cal_rouge.py bartGenerations/bart_gamma_rake/test/generations_outputs_5 5
-python cal_rouge.py bartGenerations/bart_gamma_rake/test/generations_outputs_10 10
-python cal_rouge.py bartGenerations/bart_gamma_rake/test/generations_outputs_19 19
-python cal_rouge.py bartGenerations/bart_gamma_rake/test/generations_outputs_30 30
-python cal_rouge.py bartGenerations/bart_gamma_rake/test/generations_outputs_50 50
+# python cal_rouge.py bartGenerations/bart_gamma_rake/test/generations_outputs_5 5
+# python cal_rouge.py bartGenerations/bart_gamma_rake/test/generations_outputs_10 10
+# python cal_rouge.py bartGenerations/bart_gamma_rake/test/generations_outputs_19 19
+# python cal_rouge.py bartGenerations/bart_gamma_rake/test/generations_outputs_30 30
+# python cal_rouge.py bartGenerations/bart_gamma_rake/test/generations_outputs_50 50
 
-python cal_rouge.py bartGenerations/bart_nofme_rake/test/generations_outputs_5 5
-python cal_rouge.py bartGenerations/bart_nofme_rake/test/generations_outputs_10 10
-python cal_rouge.py bartGenerations/bart_nofme_rake/test/generations_outputs_19 19
-python cal_rouge.py bartGenerations/bart_nofme_rake/test/generations_outputs_30 30
-python cal_rouge.py bartGenerations/bart_nofme_rake/test/generations_outputs_50 50
+python cal_rouge.py bartGenerations/bart_nofme/test/generations_outputs_5 5
+python cal_rouge.py bartGenerations/bart_nofme/test/generations_outputs_10 10
+python cal_rouge.py bartGenerations/bart_nofme/test/generations_outputs_19 19
+python cal_rouge.py bartGenerations/bart_nofme/test/generations_outputs_30 30
+python cal_rouge.py bartGenerations/bart_nofme/test/generations_outputs_50 50
