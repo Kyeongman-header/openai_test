c261ea9621a939db4fc21a9362d3b61dda2abd94
Kyeongman-header <zzangman2@gmail.com>
2023-11-08 19:26:52 +0900

    new sc

--- gpt.sh
+++ gpt.sh
@@ -1,40 +1,37 @@
 #!/bin/sh
-#SBATCH --job-name  gpt
-#SBATCH --time      96:00:00
-#SBATCH -c          10
-#SBATCH --mem       20G
-#SBATCH --gpus      1
-#SBATCH --mail-type END
-#SBATCH --mail-user zzangmane@snu.ac.kr
+#SBATCH __job_name  gpt
+#SBATCH __time      96:00:00
+#SBATCH _c          10
+#SBATCH __mem       20G
+#SBATCH __gpus      1
+#SBATCH __mail_type END
+#SBATCH __mail_user zzangmane@snu.ac.kr
 source activate torch
 #conda activate torch
 ml cuda
 
-#python main_gpt_embedding_savedir_logdir_isconti_laststep_usemem_usecumul_usegamma_userake_usealpha_usefusion_cumulnum_noibt_nofme_datadir_nepoch_gpuname_batch_istest_debug.py gpt_rake gpt_rake 0 0 0 0 0 0 0 0 0 0 0 whole 1 cuda:0 4 0 0
+python main_gpt_embedding_savedir_logdir_isconti_laststep_usemem_usecumul_usegamma_userake_usealpha_usefusion_cumulnum_noibt_nofme_datadir_nepoch_gpuname_batch_istest_debug.py gpt_nomemnocumul_alpha 200_gpt_rake 1 0 0 0 0 0 0 0 0 0 0 whole 1 cuda:0 4 0 0
 
-python main_gpt_embedding_savedir_logdir_isconti_laststep_usemem_usecumul_usegamma_userake_usealpha_usefusion_cumulnum_noibt_nofme_datadir_nepoch_gpuname_batch_istest_debug.py gpt_rake gpt_rake 0 0 0 0 0 0 0 0 0 0 0 whole 1 cuda:0 1 1 0
+python main_gpt_embedding_savedir_logdir_isconti_laststep_usemem_usecumul_usegamma_userake_usealpha_usefusion_cumulnum_noibt_nofme_datadir_nepoch_gpuname_batch_istest_debug.py gpt_nomemnocumul_alpha 200_gpt_rake 1 0 0 0 0 0 0 0 0 0 0 whole 1 cuda:0 1 1 0
 
-python test_longformer_filename_savedir_logdir_gpu_debug.py GPTGenerations/gpt_rake/test/generations_outputs_2 completeness-gpt gpt_2_completeness-gpt cuda:0 2 0
+python test_longformer_filename_savedir_logdir_gpu_debug.py GPTGenerations/200_gpt_rake/test/generations_outputs_5 completeness_gpt gpt_5_completeness_gpt cuda:0 5 0
 
-python test_longformer_filename_savedir_logdir_gpu_debug.py GPTGenerations/gpt_rake/test/generations_outputs_5 completeness-gpt gpt_5_completeness-gpt cuda:0 5 0
+python test_longformer_filename_savedir_logdir_gpu_debug.py GPTGenerations/200_gpt_rake/test/generations_outputs_10 completeness_gpt gpt_10_completeness_gpt cuda:0 10 0
 
-python test_longformer_filename_savedir_logdir_gpu_debug.py GPTGenerations/gpt_rake/test/generations_outputs_10 completeness-gpt gpt_10_completeness-gpt cuda:0 10 0
+python test_longformer_filename_savedir_logdir_gpu_debug.py GPTGenerations/200_gpt_rake/test/generations_outputs_19 completeness_gpt gpt_19_completeness_gpt cuda:0 19 0
 
-python test_longformer_filename_savedir_logdir_gpu_debug.py GPTGenerations/gpt_rake/test/generations_outputs_15 completeness-gpt gpt_15_completeness-gpt cuda:0 15 0
+python test_longformer_filename_savedir_logdir_gpu_debug.py GPTGenerations/200_gpt_rake/test/generations_outputs_30 completeness_gpt gpt_30_completeness_gpt cuda:0 30 0
 
-python test_longformer_filename_savedir_logdir_gpu_debug.py GPTGenerations/gpt_rake/test/generations_outputs_20 completeness-gpt gpt_20_completeness-gpt cuda:0 20 0
-
-python test_longformer_filename_savedir_logdir_gpu_debug.py GPTGenerations/gpt_rake/test/generations_outputs_25 completeness-gpt gpt_25_completeness-gpt cuda:0 25 0
+python test_longformer_filename_savedir_logdir_gpu_debug.py GPTGenerations/200_gpt_rake/test/generations_outputs_50 completeness_gpt gpt_50_completeness_gpt cuda:0 50 0
 
 
-python test_longformer_filename_savedir_logdir_gpu_debug.py GPTGenerations/gpt_rake/test/generations_outputs_2 nextsentenceprediction-gpt gpt_2_nextsentenceprediction-gpt cuda:0 2 0
 
-python test_longformer_filename_savedir_logdir_gpu_debug.py GPTGenerations/gpt_rake/test/generations_outputs_5 nextsentenceprediction-gpt gpt_5_nextsentenceprediction-gpt cuda:0 5 0
+python test_longformer_filename_savedir_logdir_gpu_debug.py GPTGenerations/200_gpt_rake/test/generations_outputs_5 nextsentenceprediction_gpt gpt_5_nextsentenceprediction_gpt cuda:0 5 0
 
-python test_longformer_filename_savedir_logdir_gpu_debug.py GPTGenerations/gpt_rake/test/generations_outputs_10 nextsentenceprediction-gpt gpt_10_nextsentenceprediction-gpt cuda:0 10 0
+python test_longformer_filename_savedir_logdir_gpu_debug.py GPTGenerations/200_gpt_rake/test/generations_outputs_10 nextsentenceprediction_gpt gpt_10_nextsentenceprediction_gpt cuda:0 10 0
 
-python test_longformer_filename_savedir_logdir_gpu_debug.py GPTGenerations/gpt_rake/test/generations_outputs_15 nextsentenceprediction-gpt gpt_15_nextsentenceprediction-gpt cuda:0 15 0
+python test_longformer_filename_savedir_logdir_gpu_debug.py GPTGenerations/200_gpt_rake/test/generations_outputs_19 nextsentenceprediction_gpt gpt_19_nextsentenceprediction_gpt cuda:0 19 0
 
-python test_longformer_filename_savedir_logdir_gpu_debug.py GPTGenerations/gpt_rake/test/generations_outputs_20 nextsentenceprediction-gpt gpt_20_nextsentenceprediction-gpt cuda:0 20 0
+python test_longformer_filename_savedir_logdir_gpu_debug.py GPTGenerations/200_gpt_rake/test/generations_outputs_30 nextsentenceprediction_gpt gpt_30_nextsentenceprediction_gpt cuda:0 30 0
 
-python test_longformer_filename_savedir_logdir_gpu_debug.py GPTGenerations/gpt_rake/test/generations_outputs_25 nextsentenceprediction-gpt gpt_25_nextsentenceprediction-gpt cuda:0 25 0
+python test_longformer_filename_savedir_logdir_gpu_debug.py GPTGenerations/200_gpt_rake/test/generations_outputs_50 nextsentenceprediction_gpt gpt_50_nextsentenceprediction_gpt cuda:0 50 0
--- plotmachine.sh
+++ plotmachine.sh
@@ -1,40 +1,36 @@
 #!/bin/sh
-#SBATCH --job-name  plotmachine
-#SBATCH --time      96:00:00
-#SBATCH -c          10
-#SBATCH --mem       20G
-#SBATCH --gpus      1
-#SBATCH --mail-type END
-#SBATCH --mail-user zzangmane@snu.ac.kr
+#SBATCH __job_name  plotmachine
+#SBATCH __time      96:00:00
+#SBATCH _c          10
+#SBATCH __mem       20G
+#SBATCH __gpus      1
+#SBATCH __mail_type END
+#SBATCH __mail_user zzangmane@snu.ac.kr
 source activate torch
 #conda activate torch
 ml cuda
 
-python main_plotmachine_embedding_savedir_logdir_isconti_laststep_usemem_usecumul_usegamma_userake_usealpha_usefusion_cumulnum_noibt_nofme_datadir_nepoch_gpuname_batch_istest_debug.py plotmachine_rake plotmachine_rake 0 0 1 0 0 0 0 0 0 0 1 whole 1 cuda:0 4 0 0 
+python main_plotmachine_embedding_savedir_logdir_isconti_laststep_usemem_usecumul_usegamma_userake_usealpha_usefusion_cumulnum_noibt_nofme_datadir_nepoch_gpuname_batch_istest_debug.py 200_plotmachine_rake 200_plotmachine_rake 0 0 1 0 0 0 0 0 0 0 1 whole 2 cuda:0 4 0 0 
 
-python main_plotmachine_embedding_savedir_logdir_isconti_laststep_usemem_usecumul_usegamma_userake_usealpha_usefusion_cumulnum_noibt_nofme_datadir_nepoch_gpuname_batch_istest_debug.py plotmachine_rake plotmachine_rake 0 0 1 0 0 0 0 0 0 0 1 whole 1 cuda:0 1 1 0
+python main_plotmachine_embedding_savedir_logdir_isconti_laststep_usemem_usecumul_usegamma_userake_usealpha_usefusion_cumulnum_noibt_nofme_datadir_nepoch_gpuname_batch_istest_debug.py 200_plotmachine_rake 200_plotmachine_rake 0 0 1 0 0 0 0 0 0 0 1 whole 1 cuda:0 1 1 0
 
-python test_longformer_filename_savedir_logdir_gpu_debug.py PlotmachineGenerations/plotmachine_rake/test/generations_outputs_2 completeness-gpt plotmachine_2_completeness-gpt cuda:0 2 0
+python test_longformer_filename_savedir_logdir_gpu_debug.py PlotmachineGenerations/200_plotmachine_rake/test/generations_outputs_5 completeness_gpt plotmachine_5_completeness_gpt cuda:0 5 0
 
-python test_longformer_filename_savedir_logdir_gpu_debug.py PlotmachineGenerations/plotmachine_rake/test/generations_outputs_5 completeness-gpt plotmachine_5_completeness-gpt cuda:0 5 0
+python test_longformer_filename_savedir_logdir_gpu_debug.py PlotmachineGenerations/200_plotmachine_rake/test/generations_outputs_10 completeness_gpt plotmachine_10_completeness_gpt cuda:0 10 0
 
-python test_longformer_filename_savedir_logdir_gpu_debug.py PlotmachineGenerations/plotmachine_rake/test/generations_outputs_10 completeness-gpt plotmachine_10_completeness-gpt cuda:0 10 0
+python test_longformer_filename_savedir_logdir_gpu_debug.py PlotmachineGenerations/200_plotmachine_rake/test/generations_outputs_19 completeness_gpt plotmachine_19_completeness_gpt cuda:0 19 0
 
-python test_longformer_filename_savedir_logdir_gpu_debug.py PlotmachineGenerations/plotmachine_rake/test/generations_outputs_15 completeness-gpt plotmachine_15_completeness-gpt cuda:0 15 0
+python test_longformer_filename_savedir_logdir_gpu_debug.py PlotmachineGenerations/200_plotmachine_rake/test/generations_outputs_30 completeness_gpt plotmachine_30_completeness_gpt cuda:0 30 0
 
-python test_longformer_filename_savedir_logdir_gpu_debug.py PlotmachineGenerations/plotmachine_rake/test/generations_outputs_20 completeness-gpt plotmachine_20_completeness-gpt cuda:0 20 0
-
-python test_longformer_filename_savedir_logdir_gpu_debug.py PlotmachineGenerations/plotmachine_rake/test/generations_outputs_25 completeness-gpt plotmachine_25_completeness-gpt cuda:0 25 0
+python test_longformer_filename_savedir_logdir_gpu_debug.py PlotmachineGenerations/200_plotmachine_rake/test/generations_outputs_50 completeness_gpt plotmachine_50_completeness_gpt cuda:0 50 0
 
 
-python test_longformer_filename_savedir_logdir_gpu_debug.py PlotmachineGenerations/plotmachine_rake/test/generations_outputs_2 nextsentenceprediction-gpt plotmachine_2_nextsentenceprediction-gpt cuda:0 2 0
+python test_longformer_filename_savedir_logdir_gpu_debug.py PlotmachineGenerations/200_plotmachine_rake/test/generations_outputs_5 nextsentenceprediction_gpt plotmachine_5_nextsentenceprediction_gpt cuda:0 5 0
 
-python test_longformer_filename_savedir_logdir_gpu_debug.py PlotmachineGenerations/plotmachine_rake/test/generations_outputs_5 nextsentenceprediction-gpt plotmachine_5_nextsentenceprediction-gpt cuda:0 5 0
+python test_longformer_filename_savedir_logdir_gpu_debug.py PlotmachineGenerations/200_plotmachine_rake/test/generations_outputs_10 nextsentenceprediction_gpt plotmachine_10_nextsentenceprediction_gpt cuda:0 10 0
 
-python test_longformer_filename_savedir_logdir_gpu_debug.py PlotmachineGenerations/plotmachine_rake/test/generations_outputs_10 nextsentenceprediction-gpt plotmachine_10_nextsentenceprediction-gpt cuda:0 10 0
+python test_longformer_filename_savedir_logdir_gpu_debug.py PlotmachineGenerations/200_plotmachine_rake/test/generations_outputs_19 nextsentenceprediction_gpt plotmachine_19_nextsentenceprediction_gpt cuda:0 19 0
 
-python test_longformer_filename_savedir_logdir_gpu_debug.py PlotmachineGenerations/plotmachine_rake/test/generations_outputs_15 nextsentenceprediction-gpt plotmachine_15_nextsentenceprediction-gpt cuda:0 15 0
+python test_longformer_filename_savedir_logdir_gpu_debug.py PlotmachineGenerations/200_plotmachine_rake/test/generations_outputs_30 nextsentenceprediction_gpt plotmachine_30_nextsentenceprediction_gpt cuda:0 30 0
 
-python test_longformer_filename_savedir_logdir_gpu_debug.py PlotmachineGenerations/plotmachine_rake/test/generations_outputs_20 nextsentenceprediction-gpt plotmachine_20_nextsentenceprediction-gpt cuda:0 20 0
-
-python test_longformer_filename_savedir_logdir_gpu_debug.py PlotmachineGenerations/plotmachine_rake/test/generations_outputs_25 nextsentenceprediction-gpt plotmachine_25_nextsentenceprediction-gpt cuda:0 25 0
+python test_longformer_filename_savedir_logdir_gpu_debug.py PlotmachineGenerations/200_plotmachine_rake/test/generations_outputs_50 nextsentenceprediction_gpt plotmachine_50_nextsentenceprediction_gpt cuda:0 50 0
